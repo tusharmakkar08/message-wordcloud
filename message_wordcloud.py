19b9ebe24b73fe1bb9007dcf9ee4a6ae008bd1d4
tusharmakkar08 <tusharmakkar08@gmail.com>
2020-03-03 22:27:58 +0530

    Implemented file reading and message formation

--- message_wordcloud.py
+++ message_wordcloud.py
@@ -1,6 +1,8 @@
 __author__ = 'Tushar Makkar <tmakkar@eightfold.ai>'
 
+import json
 import logging
+import os.path
 
 from enum import Enum
 
@@ -29,8 +31,14 @@ class MessageMedium:
         self._file_name = file_name
 
     def _read_file(self):
-        with open(self._file_name) as f:
-            return f.read()
+        extension = os.path.splitext(self._file_name)[1][1:].strip()
+        if extension == 'txt':
+            with open(self._file_name) as f:
+                return f.read()
+        elif extension == 'json':
+            with open(self._file_name) as f:
+                return json.load(f)
+        return None
 
     def convert_to_messages(self):
         return
@@ -38,22 +46,69 @@ class MessageMedium:
 
 class FacebookMessageMedium(MessageMedium):
     def convert_to_messages(self):
-        pass
+        data = self._read_file()
+        actual_data = data['messages']
+        list_of_messages = []
+        for i in actual_data:
+            if i.get('content'):
+                list_of_messages.append(Message(i['content'], i['sender_name']))
+        return list_of_messages
 
 
 class WhatsappMessageMedium(MessageMedium):
     def convert_to_messages(self):
-        pass
+        data = self._read_file()
+        chat_data = data.split('\n')[1:]
+        participants = set()
+        for data in chat_data:
+            if len(participants) >= 2:
+                break
+            participants.add(data.split(' - ')[-1].split(':')[0])
+        participants = list(participants)
+        participants = [i + ': ' for i in participants]
+        list_of_messages = []
+        for data in chat_data:
+            sender = None
+            for i in participants:
+                if i in data:
+                    sender = i
+                    break
+            if data and sender:
+                list_of_messages.append(Message(data.split(sender)[-1], sender[:-2]))
+        return list_of_messages
 
 
 class InstagramMessageMedium(MessageMedium):
-    def convert_to_messages(self):
-        pass
+    def convert_to_messages(self, participants=None):
+        data = self._read_file()
+        actual_data = None
+        for i in data:
+            if participants == i['participants']:
+                actual_data = i
+                break
+        list_of_messages = []
+        for i in actual_data['conversation']:
+            if i.get('text'):
+                list_of_messages.append(Message(i.get('text'), i.get('sender')))
+        return list_of_messages
 
 
 class HikeMessageMedium(MessageMedium):
     def convert_to_messages(self):
-        pass
+        data = self._read_file()
+        line_data = data.split('\n')
+        chat_data = line_data[1:]
+        participants = ['me-', line_data[0].split('Chat with ')[-1] + '-']
+        list_of_messages = []
+        for data in chat_data:
+            sender = None
+            for i in participants:
+                if i in data:
+                    sender = i
+                    break
+            if data and sender:
+                list_of_messages.append(Message(data.split(sender)[-1], sender[:-1]))
+        return list_of_messages
 
 
 class MessageWordCloud:
@@ -61,10 +116,17 @@ class MessageWordCloud:
         self._logger = logger
         self._data_map = data_map
         self._class_map = {
+            MessageCategory.WHATSAPP: WhatsappMessageMedium,
+            MessageCategory.HIKE: HikeMessageMedium,
+            MessageCategory.FACEBOOK: FacebookMessageMedium,
+            MessageCategory.INSTAGRAM: InstagramMessageMedium
         }
 
     def build_final_data_js_file(self):
-        return
+        data = HikeMessageMedium(self._data_map[MessageCategory.HIKE]).convert_to_messages()
+        data = WhatsappMessageMedium(self._data_map[MessageCategory.WHATSAPP]).convert_to_messages()
+        data = InstagramMessageMedium(self._data_map[MessageCategory.INSTAGRAM]).convert_to_messages(participants=["a", "b"])
+        data = FacebookMessageMedium(self._data_map[MessageCategory.FACEBOOK]).convert_to_messages()
 
 
 if __name__ == '__main__':
